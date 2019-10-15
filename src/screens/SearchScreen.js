f8c41dd1aad3fdcb0cb4c246b4fcf102960097cc
Oscar Garcia <oscarantoniox10@gmail.com>
2019-10-14 19:54:56 -0700

    Terminado

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,8 +8,6 @@ const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
 
-  //console.log(results);
-
   const filterResultsByPrice = price => {
     return results.filter(result => {
       return result.price === price;
